d4783e0e9f0c21b28d63694b28c078dfc2d227f2
tzchung pan <louis8598@gmail.com>
2020-03-22 02:01:40 +0800

    password_check_tool

---
+++ password_test.py
@@ -0,0 +1,26 @@
+#ver1 寫法一 我自己想的
+code = 'a123456'
+number = 3 #剩餘機會
+while number >= 1:
+    pwd = input('請輸入密碼，最多輸入3次:')
+    if code == pwd:
+        print('密碼正確') #逃出迴圈
+        break
+    else:
+        number = number - 1
+        print('密碼錯誤! 還有', number,'次機會')
+
+
+#ver2 寫法二 老師教的寫法
+code = 'a123456'
+number = 3 #剩餘機會
+while True:
+    pwd = input('請輸入密碼，最多輸入3次:')
+    if code == pwd:
+        print('密碼正確') #逃出迴圈
+        break
+    else:
+        number = number - 1
+        print('密碼錯誤! 還有', number,'次機會')
+        if number = 0:
+            break
