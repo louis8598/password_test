2ca150c13a2bfae680ecddcfc33cc9a30486593b
tzchung pan <louis8598@gmail.com>
2020-03-22 02:19:06 +0800

    最終調整版本

--- password_test.py
+++ password_test.py
@@ -1,45 +1,4 @@
-#寫法一 我自己想的
-code = 'a123456'
-number = 3 #剩餘機會
-while number >= 1:
-    pwd = input('請輸入密碼，最多輸入3次:')
-    if code == pwd:
-        print('密碼正確') #逃出迴圈
-        break
-    else:
-        number = number - 1
-        print('密碼錯誤! 還有', number,'次機會')
-
-
-
-
-#寫法二 老師教的寫法
-code = 'a123456'
-number = 3 #剩餘機會
-while True:
-    pwd = input('請輸入密碼，最多輸入3次:')
-    if code == pwd:
-        print('密碼正確') #逃出迴圈
-        break
-    else:
-        number = number - 1
-        print('密碼錯誤! 還有', number,'次機會')
-        if number = 0:
-            break
-
-#寫法三 老師教的寫法，當不能使用while True的時候怎麼辦
-code = 'a123456'
-number = 3 #剩餘機會
-while number > 0:
-    pwd = input('請輸入密碼，最多輸入3次:')
-    if code == pwd:
-        print('密碼正確') #逃出迴圈
-        break
-    else:
-        number = number - 1
-        print('密碼錯誤! 還有', number,'次機會')
-
-#寫法四 老師教的寫法，進行微調
+#最終版 老師教的寫法，進行微調--->就用這個
 code = 'a123456'
 number = 3 #剩餘機會
 while number > 0:
@@ -50,5 +9,5 @@ while number > 0:
         break
     else:
         print('密碼錯誤!')
-        if number > 0 #不要印出密碼錯誤還有0次
+        if number > 0: #不要印出密碼錯誤還有0次
             print('還有', number,'次機會')
